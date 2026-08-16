8adc08a0a65b395aa27272360bf79bc82ba82b0c
arwa-82 <test19hey@gmail.com>
2026-08-16 17:47:13 +0200

    fix double function app in App.jsx

--- Events_Api_Project/src/App.jsx
+++ Events_Api_Project/src/App.jsx
@@ -1,13 +1,12 @@
 import { Routes, Route } from "react-router";
 import MainLayout from "./layouts/MainLayout.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
 import Home from "./pages/Home.jsx";
 import SignIn from "./pages/SignIn.jsx";
 import SignUp from "./pages/SignUp.jsx";
 import CreateEvent from "./pages/CreateEvent.jsx";
 import EventDetails from "./pages/EventDetails.jsx";
 import NotFound from "./pages/NotFound.jsx";
-function App() {
-  const [count, setCount] = useState(0);
 
 function App() {
   return (
